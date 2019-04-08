85fde612f79a7f316f2d5bcdcc7f7a1758a1d095
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-08 14:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function BoardMember (name, homeState, training) {
   }
   
   this.sayHi = function() {
-    return `Hi, my name is ${this.name}.`
+    return `Hi, my name is ${this.name}. I wam from ${this.homeState}, and `
   }
 }
