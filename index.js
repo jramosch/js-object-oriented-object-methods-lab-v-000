e20442996a219d225fbea84a1be6c93c9d03d6f7
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-08 14:32:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-function BoardMember (name, homeState, training)
+function BoardMember (name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  
+  this.
+}
